4fc4b4f325962e94a5977907cafab694ea32c0d7
Josue Mancilla <jujomago@gmail.com>
2017-01-25 23:17:03 -0400

    sended

---
+++ Entradas.java
@@ -0,0 +1,38 @@
+
+public class Entradas{
+    String name="";
+    String desc="";
+    String link="";
+    
+	public Entradas() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setName(String text) {
+		// TODO Auto-generated method stub
+	this.name=text;	
+	}
+
+	public void setDescription(String text) {
+		// TODO Auto-generated method stub
+	this.desc=text;	
+	}
+
+	public void setlink(String text) {
+		// TODO Auto-generated method stub
+	this.link=text;	
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+	return this.name;
+	}
+	public String getDescription(){
+		return this.desc;
+	}
+	public String getLink(){
+		return this.link;
+	}
+
+}
